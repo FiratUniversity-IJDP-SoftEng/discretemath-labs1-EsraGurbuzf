9605b837c86f44746fdf9282fb19bdd4da35b353
EsraGurbuzf <230543001@firat.edu.tr>
2025-11-12 15:34:39 +0300

    Update ex-3.py

--- ex-3.py
+++ ex-3.py
@@ -1,1 +1,13 @@
+def squareminusone(number3):
+  primelist = allprime(100)
+  for i in primelist:
+    is_prime2 = True
+    for j in range(2, i + 1):
+      if i % j == 0:
+        is_prime2 = False
+    if is_prime2:
+      primelist.append(i)
+  return primelist
 
+tell = int(input("Enter a number: "))
+print(squareminusone(tell))

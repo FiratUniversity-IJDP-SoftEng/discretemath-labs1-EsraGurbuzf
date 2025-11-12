5226ea3e8770533b5bf8a453b0ad38d28cbd7d6c
EsraGurbuzf <230543001@firat.edu.tr>
2025-11-12 15:34:17 +0300

    Update ex-2.py

--- ex-2.py
+++ ex-2.py
@@ -1,1 +1,13 @@
+def allprime(number2):
+  primes = []
+  for i in range(2, number2 + 1):
+    is_prime = True
+    for j in range(2, int(i ** 0.5) + 1):
+      if i % j == 0:
+        is_prime = False
+    if is_prime:
+      primes.append(i)
+  return primes
 
+ask = int(input("Enter a number: "))
+print(allprime(ask))
